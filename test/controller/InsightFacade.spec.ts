5504840f6b74382a0f06ec642800f6660f6bce6c
Hamsterino <116461609+AndrewYBLin@users.noreply.github.com>
2026-05-14 13:41:31 -0700

    checking git again

--- test/controller/InsightFacade.spec.ts
+++ test/controller/InsightFacade.spec.ts
@@ -4,7 +4,7 @@ import { clearDisk, getContentFromArchives, loadTestQuery } from "../TestUtil";
 
 import { expect, use } from "chai";
 import chaiAsPromised from "chai-as-promised";
-// wow does git work? frfr
+// testing git again
 use(chaiAsPromised);
 
 export interface ITestQuery {
