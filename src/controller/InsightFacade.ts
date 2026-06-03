28159fdc4109ca9c087bc9c45da5c777ca680974
hamsters <hamsters@student.ubc.ca>
2026-06-03 11:56:46 -0700

    testing if I didn't break git

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -17,7 +17,7 @@ import * as fs from "fs-extra";
 export default class InsightFacade implements IInsightFacade {
 	private datasets: Map<string, InsightDataset>;
 	private currentQueryId: string;
-	// mann
+	// TESTING IF GIT DIDN'T BREAK
 	constructor() {
 		this.datasets = new Map<string, InsightDataset>();
 		this.currentQueryId = "";
